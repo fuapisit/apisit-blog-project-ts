58be5034dab0da95769a3ffafe2fb8c9457a8d07
Apisit <fu.apisit@gmail.com>
2025-06-15 21:15:20 +0700

    fix: adjust alias config and import paths to fix deploy error

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-<<<<<<< Updated upstream
-// https://vite.dev/config/
-=======
-
->>>>>>> Stashed changes
 export default defineConfig({
   plugins: [react()],
 })
